d2cc212871f0a66f828574741b7f2cb318a50ba8
yxoussef1 <youssefamir1172@gmail.com>
2023-05-27 02:20:04 +0300

    Youssef Amir Final

--- BankerP1JS.js
+++ BankerP1JS.js
@@ -2,6 +2,10 @@ const summaryButton = document.getElementById("summary");
 const CCButton = document.getElementById("CC");
 const loansButton = document.getElementById("loans");
 const ComplaintsButton = document.getElementById("Complaints");
+const ManageButtonb1 = document.getElementById("b1");
+const ManageButtonb2 = document.getElementById("b2");
+const ManageButtonb3 = document.getElementById("b3");
+
 
 const summaryPage = document.getElementById("summaryPage");
 const cardsPage = document.getElementById("cardsPage");
@@ -12,6 +16,15 @@ const summaryIcon = document.getElementById("houseIcon");
 const CCIcon = document.getElementById("CCIcon");
 const loansIcon = document.getElementById("pigIcon");
 const ComplaintsIcon = document.getElementById("chatIcon");
+
+const approveButton = document.getElementById("bTa");
+const declineButton = document.getElementById("bTd");
+const table = document.getElementById("loanstrial");
+const table2 = document.getElementById("loanstrial2");
+const row2 = table2.getElementsByTagName("tr");
+const row = table.getElementsByTagName("tr");
+const table3 = document.getElementById("loanstrial3");
+const row3 = table3.getElementsByTagName("tr");
 
 function switchSummary() {
 
@@ -25,7 +38,7 @@ function switchSummary() {
 
         CCIcon.classList.replace('bi-credit-card-2-back-fill','bi-credit-card-2-back');
         loansIcon.classList.replace('bi-piggy-bank-fill','bi-piggy-bank');
-        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots-fill');
+        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots');
         
         summaryPage.classList.replace('hide','show');
         summaryButton.classList.replace('notclicked','clicked');
@@ -43,7 +56,7 @@ function switchRequests() {
         summaryButton.classList.replace('clicked','notclicked');
 
         loansIcon.classList.replace('bi-piggy-bank-fill','bi-piggy-bank');
-        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots-fill');
+        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots');
         summaryIcon.classList.replace('bi-house-fill','bi-house');
 
         cardsPage.classList.replace('hide','show');
@@ -61,7 +74,7 @@ function switchLoans(){
         summaryButton.classList.replace('clicked','notclicked');
         CCButton.classList.replace('clicked','notclicked');
         
-        ComplaintsIcon.classList.replace('bi-credit-card-2-back-fill','bi-credit-card-2-back');
+        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots');
         summaryIcon.classList.replace('bi-house-fill','bi-house');
         CCIcon.classList.replace('bi-credit-card-2-front-fill','bi-credit-card-2-front');
         
@@ -87,3 +100,185 @@ function switchComplaints(){
         billsPage.classList.replace('hide','show');
         ComplaintsButton.classList.replace('notclicked','clicked');
         ComplaintsIcon.classList.replace('bi-chat-right-dots','bi-chat-right-dots-fill');}
+
+function switchManage(){
+        cardsPage.classList.replace('show','hide');
+        billsPage.classList.replace('show','hide');
+        summaryPage.classList.replace('show','hide');
+        
+        ManageButtonb1.classList.replace('clicked','notclicked');
+        ManageButtonb2.classList.replace('clicked','notclicked');
+        
+        
+        ComplaintsIcon.classList.replace('bi-chat-right-dots-fill','bi-chat-right-dots');
+        summaryIcon.classList.replace('bi-house-fill','bi-house');
+        CCIcon.classList.replace('bi-credit-card-2-front-fill','bi-credit-card-2-front');
+        
+        loansPage.classList.replace('hide','show');
+        ManageButtonb1.classList.replace('notclicked','clicked');
+        loansIcon.classList.replace('bi-piggy-bank','bi-piggy-bank-fill');
+        switchLoans();
+}
+
+function switchApprove(id){
+        row[id].classList.add("slowlyhideA");
+        setTimeout(() => {
+            row[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row.length;i++){
+                    if(!(row[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row[i].classList.add("oddRow");
+                    }
+                    if(!row[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
+function switchDecline(id){
+        row[id].classList.add("slowlyhideD");
+        setTimeout(() => {
+            row[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row.length;i++){
+                    if(!(row[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row[i].classList.add("oddRow");
+                    }
+                    if(!row[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
+function switchApprove2(id){
+        row2[id].classList.add("slowlyhideA");
+        setTimeout(() => {
+            row2[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row2.length;i++){
+                    if(!(row2[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row2[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row2[i].classList.add("oddRow");
+                    }
+                    if(!row2[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row2[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
+function switchDecline2(id){
+        row2[id].classList.add("slowlyhideD");
+        setTimeout(() => {
+            row2[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row2.length;i++){
+                    if(!(row2[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row2[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row2[i].classList.add("oddRow");
+                    }
+                    if(!row2[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row2[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
+function switchApprove3(id){
+        row3[id].classList.add("slowlyhideA");
+        setTimeout(() => {
+            row3[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row3.length;i++){
+                    if(!(row3[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row3[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row3[i].classList.add("oddRow");
+                    }
+                    if(!row3[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row3[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
+function switchDecline3(id){
+        row3[id].classList.add("slowlyhideD");
+        setTimeout(() => {
+            row3[id].classList.add("hide")
+            var found=false;
+            var j=1;
+            for(i=1;i<row3.length;i++){
+                    if(!(row3[i].classList.contains("hide"))){
+                            found=true;
+                    }
+                    if(j%2==0){
+                        row3[i].classList.remove("oddRow");
+                    }
+                    else{
+                        row3[i].classList.add("oddRow");
+                    }
+                    if(!row3[i].classList.contains("hide")){
+                        j++;
+                    }
+            }
+            if(!found){
+                row3[0].classList.add("hide");
+            }
+            // 👇️ hides element (still takes up space on page)
+            // box.style.visibility = 'hidden';
+          }, 500);
+}
